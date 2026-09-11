d3cb829717fb7ee46923b0c7d8384cfda8e58158
Arman <armankhorajiyask@gmail.com>
2026-09-11 10:17:29 +0530

    Add array practice

--- 01_python_basics/08_recursion.py
+++ 01_python_basics/08_recursion.py
@@ -9,8 +9,103 @@
 # count_down(5);
 
 # Factorial Program:
-def factorial(n):
-    if n==0:
-        return 1;
-    return n * factorial(n - 1)
-print(factorial(5));
+# def factorial(n):
+#     if n==0:
+#         return 1;
+#     return n * factorial(n - 1)
+# print(factorial(5));
+
+# Sum of 1 to n Numbers:
+# def sum_n(n):
+#     if n==0:
+#         return 0;
+#     return n+sum_n(n-1);
+# print(sum_n(5));
+
+# Q1
+# def count_down(n):
+#     if n==0:
+#         return;
+#     print(n);
+#     count_down(n-1);
+# count_down(5);
+
+# Q2
+# def factorial(n):
+#     if n==0:
+#         return 1;
+#     return n*factorial(n-1);
+# print(factorial(5));
+
+# Q3
+# def sum_n(n):
+#     if n==0:
+#         return 0;
+#     return n+sum_n(n-1);
+# print(sum_n(5));
+
+# Q4
+# def power(a,b):
+#     if b==0:
+#         return 1;
+#     return a*power(a,b-1);
+# print(power(2,4));
+
+# Q5
+# First
+# test(4)
+# 4==0 F
+# return 4 + test(3)
+# test(3)
+# 3==0 F
+# return 3 + test(2)
+# test(2)
+# 2==0 F
+# return 2 + test(1)
+# 1==0 F
+# return 1 +test(0)
+# test(0)
+# 0==0 T
+# Now, test(0)=1 and so on
+# then answer is 10
+
+# ex: factorial
+# def factorial(n):
+#     if n==0:
+#         return 1;
+#     return n * factorial(n-1);
+# print(factorial(5));
+
+# FIBONACCI SERIES(0 1 1 2 3 5 8 13 21.....):
+# def fib(n):
+#     if n==0:
+#         return 0;
+#     if n==1:
+#         return 1;
+#     return fib(n-1) + fib(n-2);
+# # print(fib(40));
+
+# Q1
+# def fibo(n):
+#     if n==0:
+#         return 0;
+#     if n==1:
+#         return 1;
+#     return fibo(n-1)+fibo(n-2);
+# print(fibo(45));
+
+# Q2
+# fib(4) 4==0 F 4==1 F
+# return fib(3)+fibp(2)
+# fib(3) 3==0 F 3==1 F  fibo(2) 2==0 F 2==1 F
+# return fib(2)+fib(1)  return fib(1)+fib(0)
+# fib(2) 2==0 F 2==1 F  fib(1) 1==0 F 1==1 T    fib(1) 1==0 F 1==1 T    fib(0) 0==0 T return 0
+# return fib(1)+fib(0)  return 1                return 1
+
+# Q3
+# def sum_digits(n):
+#     if n==0:
+#         return 0;
+    
+#     return(n%10)+sum_digits(n//10);
+# print(sum_digits(1234));
---
+++ 01_python_basics/09_strings.py
@@ -0,0 +1,54 @@
+# Creating a String
+# name="Arman";
+
+# Indexing
+# s="python";
+# print(s[0]);
+# print(s[1]);
+# print(s[2]);
+
+# Negative Indexing:
+# s="python";
+# print(s[-1]);
+# print(s[-1]);
+
+# Slicing
+# Remember: Start Included, Stop Excluded
+# s[start:stop]
+
+# s="python";
+# print(s[0:3]);
+
+# Reverese a String:
+# s="python"
+# print(s[::-1]);
+
+# len():
+# s="python";
+# print(len(s));
+
+# Loop through a String:
+# s="python";
+# for ch in s:
+#     print(ch);
+
+# or using indexes:
+# s="python";
+# for i in range(len(s)):
+#     print(s[i]);
+
+# Check Character with in:
+# s="python";
+# print("p" in s);
+# print("z" in s);
+
+# lower() and upper():
+# s="python"
+# print(s.lower());
+# print(s.upper());
+
+# split()
+# s="10 20 30 40";
+# arr=s.split();
+# print(arr);
+
