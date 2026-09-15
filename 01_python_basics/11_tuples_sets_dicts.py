d0f13074100639e435cfa22f4b1daacd8efd5667
Arman <armankhorajiyask@gmail.com>
2026-09-15 10:29:52 +0530

    Update Python basics

--- 01_python_basics/11_tuples_sets_dicts.py
+++ 01_python_basics/11_tuples_sets_dicts.py
@@ -69,15 +69,49 @@
 
 # Dictionaries are very Important in DSA:
 # Suppose:
-arr = [2, 5, 2, 8, 2, 5]
+# arr = [2, 5, 2, 8, 2, 5]
 
 # We want to count each number.
 # Using a dictionary:
 
-frequency = {}
-for num in arr:
-    if num in frequency:
-        frequency[num] += 1
-    else:
-        frequency[num] = 1
-print(frequency)
+# frequency = {}
+# for num in arr:
+#     if num in frequency:
+#         frequency[num] += 1
+#     else:
+#         frequency[num] = 1
+# print(frequency);
+
+# Q1
+# point=(10,20,30);
+# print(point[1]);
+
+# Q2
+# nums=[1,2,2,3,3,4,4,5];
+# a=set(nums);
+# print(a);
+
+# Q3
+# nums={1,3,5,7,9};
+# print(7 in nums);
+
+# Q4
+# student={
+#     "name":"Arman",
+#     "age":18,
+#     "marks":85
+# }
+# print(student["marks"]);
+# I understand this but how do we print specific students marks if there are more than one.
+
+# Q5
+# Find the frequency of every number:
+# arr=[1,2,1,3,2,1,4];
+# frequency={};
+# for i in arr:
+#     if i in frequency:
+#         frequency[i]+=1;
+#     else:
+#         frequency[i]=1;
+# print(frequency);
+
