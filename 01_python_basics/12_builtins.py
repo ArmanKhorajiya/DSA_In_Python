0e7f3f6b059e7d8f01bcb8ce56c379bf9de33a9c
Arman <armankhorajiyask@gmail.com>
2026-09-15 10:54:31 +0530

    Update Python basics

---
+++ 01_python_basics/12_builtins.py
@@ -0,0 +1,74 @@
+# len():
+# Finds the number of elements.
+# arr=[10,20,30,40];
+# print(len(arr));
+
+# max() and min():
+# arr=[10,5,30,15];
+# print(max(arr));
+# print(min(arr));
+
+# sum():
+# arr=[10,20,30];
+# print(sum(arr));
+
+# sorted()
+# Creates a new sorted list.
+# arr=[5,2,8,1];
+# new_arr=sorted(arr);
+# print(new_arr);
+# print(arr);
+
+# arr.sort()
+# changes the original list.
+
+# sorted(arr)
+# returns a new sorted list.
+
+# enumerate():
+# Instead of:
+# arr=[10,20,30];
+# for i in range(len(arr)):
+#     print(i,arr[i]);
+# # you can write:
+# for i, value in enumerate(arr):
+#     print(i,value);
+
+# zip():
+# Used to process two lists together:
+# names=["Arman","Rahul","Amit"];
+# marks=[85,90,78];
+# for name,marks in zip(names,marks):
+#     print(name,marks);
+
+# abs():
+# Returns the positive value of a number.
+# print(abs(-10));
+
+# ⭐ Most important for DSA
+# len()       → size
+# max()       → maximum
+# min()       → minimum
+# sum()       → total
+# sorted()    → sorted copy
+# enumerate() → index + value
+# zip()       → two lists together
+# abs()       → absolute value
+
+# arr=[10,25,5,40,15];
+# Q1
+# print(max(arr));
+
+# Q2
+# print(min(arr));
+
+# Q3
+# print(sum(arr));
+
+# Q4
+# arr.sort();
+# print(arr);
+
+# Q5
+# for i,value in enumerate(arr):
+#     print(i,value)
