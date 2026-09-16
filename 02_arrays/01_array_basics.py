848a814658d3d2f332fd57517c1c28c82f068ff3
Arman <armankhorajiyask@gmail.com>
2026-09-16 11:52:23 +0530

    Update Python basics

--- 02_arrays/01_array_basics.py
+++ 02_arrays/01_array_basics.py
@@ -68,3 +68,153 @@
 #     if i>0:
 #         total+=i;
 # print(total);
+
+
+# Linear Search:
+# Linear search means checking elements one by one until we find the target.
+# arr=[10,25,5,40,15];
+# target=40;
+# found=False
+# for i in range(len(arr)):
+#     if arr[i]==target:
+#         print("Found at index",i);
+#         found=True;
+#         break;
+
+# if not found:
+#     print("Not Found");
+
+# Search and Count:
+# Sometimes we need to find how many times a target appears.
+# arr=[2,5,2,8,2,5];
+# count=0;
+# target=2;
+# for i in range(len(arr)):
+#     if arr[i]==target:
+#         count+=1
+#         print("Found at Index:",i)
+#         continue
+# print(count);
+
+# Q1
+# arr=[10,20,30,40,50];
+# target=30;
+# for i in range(len(arr)):
+#     if arr[i]==target:
+#         print(i)
+#         break;
+
+# Q2
+# arr=[10,20,30,40,50];
+# target=100;
+# Found=False;
+# for i in arr:
+#     if i==target:
+#         Found=True
+#         print("Found");
+# if Found==False:
+#     print("Not Found");
+
+# Q3
+# arr=[5,2,5,8,5,5,10];
+# count=0;
+# target=5;
+# for i in arr:
+#     if i==target:
+#         count+=1;
+# print(count);
+
+# Q4
+# arr=[3,7,2,7,9,7];
+# target=7;
+# for i in range(len(arr)):
+#     if arr[i]==target:
+#         print("Found at index:",i)
+#         break;
+
+
+# Insertion and Deletion:
+# Insertion
+# arr=[10,20,30,40,50];
+# arr.insert(2,25);
+# print(arr);
+
+# Deletion
+# arr=[10,20,30,40];
+# arr.pop(1);
+# print(arr);
+
+# Q1
+# arr=[10,20,30,40];
+# arr.insert(2,25);
+# print(arr);
+
+# Q2
+# arr=[10,20,30,40];
+# arr.pop(1);
+# print(arr);
+
+# Q3
+# arr=[10,20,30];
+# arr.insert(0,5);
+# print(arr);
+
+# Q4
+# arr=[10,20,30,20,40];
+# target=20;
+# for i in range(len(arr)):
+#     if arr[i]==target:
+#         arr.pop(i)
+#         break;
+# print(arr);
+
+
+# Array Traversal Patterns
+# Find an element satisfyiing a condition:
+# Find the first even number
+# arr=[7,9,11,8,15,20];
+# for i in arr:
+#     if i%2==0:
+#         print("Found:",i)
+#         break;
+
+# Find the first positive number
+# arr=[-5,-2,0,7,10];
+# for i in arr:
+#     if i>0:
+#         print("Found:",i)
+#         break;
+
+# Q1
+# arr=[2,4,8,10,7,9];
+# for i in arr:
+#     if i%2==1:
+#         print("Found:",i)
+#         break;
+
+# Q2
+# arr=[5,8,3,-4,-10];
+# for i in arr:
+#     if i<0:
+#         print("Found:",i)
+#         break;
+
+# Q3
+# arr=[5,2,7,5,9,5];
+# target=5;
+# for i in range(len(arr)-1,-1,-1):
+#     if arr[i]==target:
+#         print("Found at:",i);
+
+# Q4
+# arr=[10,5,20,8,20,15];
+# largest=float("-inf");
+# second=float('-inf');
+# for i in arr:
+#     if i>largest:
+#         second=largest
+#         largest=i;
+#     elif i>second and i!=largest:
+#         second=i;
+# print(second);
+
