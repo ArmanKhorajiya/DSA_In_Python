a1494db076e5e25c2bb7577a7a387bda4487a3b0
Arman <armankhorajiyask@gmail.com>
2026-09-21 13:34:48 +0530

    Update Python basics

---
+++ 02_arrays/05_product_except_self.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def productExceptSelf(self,nums):
+
+        product=1
+        left=[1]*len(nums)
+
+        for i in range(len(nums)):
+            left[i]=product
+            product=product*nums[i]
+
+        product=1
+        right=[1]*len(nums)
+
+        for i in range(len(nums)-1,-1,-1):
+            right[i]=product
+            product=product*nums[i]
+
+        answer=[]
+        for i in range(len(nums)):
+            answer.append(left[i]*right[i])
+        return answer
+solution=Solution()
+print(solution.productExceptSelf([1,2,3,4]))
